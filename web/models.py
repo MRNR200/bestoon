4562d994bc77ec5e7dc59e885726974ca3304a2d
MRNF <mohammadrezanajafianfar@gmail.com>
2024-11-20 18:21:31 +0330

    expense model is there

--- web/models.py
+++ web/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+from django.contrib.auth.models import User
+from django.utils import timezone
+now = timezone.now()
 # Create your models here.
+
+class Expense(models.Model):
+    text = models.CharField(max_length=255)
+    date = models.DateTimeField()
+    amount = models.BigIntegerField()
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
